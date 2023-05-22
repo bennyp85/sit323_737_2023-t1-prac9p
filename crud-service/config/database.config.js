9a2362fce36cf8a9abc533d940a5de291a96d95f
bennyp85 <benjamintp.1985@gmail.com>
2023-05-22 22:36:34 +1000

    final

--- crud-service/config/database.config.js
+++ crud-service/config/database.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017'
-}
+    url: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017',
+  };
+  
--- crud-service/server.js
+++ crud-service/server.js
@@ -15,14 +15,14 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // Connecting to the database
-mongoose.connect(dbConfig.url, {
-	useNewUrlParser: true
-}).then(() => {
-    console.log("Successfully connected to the database");
-}).catch(err => {
+mongoose.connect(dbConfig.url)
+  .then(() => {
+    console.log('Successfully connected to the database');
+  })
+  .catch(err => {
     console.log('Could not connect to the database. Exiting now...', err);
     process.exit();
-});
+  });
 
 require('./routes/routes.js')(app);
 
